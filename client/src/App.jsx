c36fcae6c946dffc5b0147639f31ddf0b1df589c
SmallPeenwow <bowdencamryn@gmail.com>
2022-04-27 14:41:45 +0200

    background done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen flex flex-col text-white text-center justify-center bg-main-background ">
       <header>
-        <h3 className="">Tic Tac Toe</h3>  
+        <h3 className="text-3xl font-medium mb-2">Tic Tac Toe</h3>  
       </header>
       <button>Start New</button>
       <button>Join Game</button>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     extend: {
       ontFamily: {
         sans: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'main-blue': '#011f4b',
+        'secondary-blue': '#00518a',
+      }, 
+      backgroundImage: {
+        'main-background': "linear-gradient(to bottom, #011f4b 85%, #00518a);",
       }
     },
   },
