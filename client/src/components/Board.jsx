6a8cd38d9a3bb38e5c96e4fdb461f260d734ae7d
SmallPeenwow <bowdencamryn@gmail.com>
2022-05-03 09:53:43 +0200

    Board styling done and functionality working on

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Board = () => {
+  
+  const [setTurn, isSetTurn] = useState('X');
+
+  const [setGameEnded, isSetGameEnded] = useState(false);
+
+  const clicked = (event) => {
+    event.target.innerText = setTurn;
+    isSetTurn(setTurn == 'X' ? 'O' : 'X');
+  };
+
   return (
-    <div>Board</div>
+    <div className="flex w-72 flex-wrap mt-6 mb-6" onClick={(e) => clicked(e)}>
+      <div className="square border-r-2 border-b-2"></div>
+      <div className="square border-b-2 border-r-2"></div>
+      <div className="square border-b-2"></div>
+      <div className="square border-b-2 border-r-2"></div>
+      <div className="square border-b-2 border-r-2"></div>
+      <div className="square border-b-2"></div>
+      <div className="square border-r-2"></div>
+      <div className="square border-r-2"></div>
+      <div className="square"></div>
+    </div>
   )
 }
 
--- client/src/pages/GameArea.jsx
+++ client/src/pages/GameArea.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Board from '../components/Board'
 
 const GameArea = ({isDisplay, isPlayerName, resetInputField, landingPageDisplay}) => {
   return (
     <div className={isDisplay == 3 ? "flex flex-col justify-center h-full" : "hidden"}>
         <h1 className="text-5xl">{isPlayerName}</h1>
-
+        <Board />
         <h2 className="border-b-2 border-b-white w-72 text-2xl">Score Board</h2>
         <div className="flex justify-center text-xl">
             <div className="flex flex-col p-2 px-4 w-28 overflow-hidden">
