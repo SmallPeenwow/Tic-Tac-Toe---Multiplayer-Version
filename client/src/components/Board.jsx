4de225103be48999118768c4ae727da3e3b684af
SmallPeenwow <bowdencamryn@gmail.com>
2022-05-04 13:36:29 +0200

    Created statement to check if square is filled

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -3,26 +3,40 @@ import React, { useState } from 'react'
 
 const Board = () => {
   
-  const [setTurn, isSetTurn] = useState('X');
-
-  const [setGameEnded, isSetGameEnded] = useState(false);
+  const [state, setState] = useState({ 
+    turn: 'X', 
+    gameEnded: false, 
+    board: Array(9).fill('')
+  });
 
   const clicked = (event) => {
-    event.target.innerText = setTurn;
-    isSetTurn(setTurn == 'X' ? 'O' : 'X');
+
+    if(state.board[event.target.dataset.square] == ''){
+
+      state.board[event.target.dataset.square] = state.turn;
+  
+      event.target.innerText = state.turn;
+      setState({
+        turn: state.turn == 'X' ? 'O' : 'X',
+        board: state.board,
+      })
+      
+    }
+
+    console.log(state.board);
   };
 
   return (
-    <div className="flex w-72 flex-wrap mt-6 mb-6" onClick={(e) => clicked(e)}>
-      <div className="square border-r-2 border-b-2"></div>
-      <div className="square border-b-2 border-r-2"></div>
-      <div className="square border-b-2"></div>
-      <div className="square border-b-2 border-r-2"></div>
-      <div className="square border-b-2 border-r-2"></div>
-      <div className="square border-b-2"></div>
-      <div className="square border-r-2"></div>
-      <div className="square border-r-2"></div>
-      <div className="square"></div>
+    <div className="flex w-72 flex-wrap mt-6 mb-6 cursor-pointer" onClick={(e) => clicked(e)}>
+      <div className="square border-r-2 border-b-2" data-square="0"></div>
+      <div className="square border-b-2 border-r-2" data-square="1"></div>
+      <div className="square border-b-2" data-square="2"></div>
+      <div className="square border-b-2 border-r-2" data-square="3"></div>
+      <div className="square border-b-2 border-r-2" data-square="4"></div>
+      <div className="square border-b-2" data-square="5"></div>
+      <div className="square border-r-2" data-square="6"></div>
+      <div className="square border-r-2" data-square="7"></div>
+      <div className="square" data-square="8"></div>
     </div>
   )
 }
