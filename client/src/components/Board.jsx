5c69e64fa392f3ccafd93a4a67145af719faf97f
SmallPeenwow <bowdencamryn@gmail.com>
2022-05-04 21:32:42 +0200

    Need work maybe

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -6,25 +6,65 @@ const Board = () => {
   const [state, setState] = useState({ 
     turn: 'X', 
     gameEnded: false, 
-    board: Array(9).fill('')
+    board: Array(9).fill(''),
+    totalMoves: 0,
+    winner: undefined,
   });
 
   const clicked = (event) => {
 
+    let num = event.target.dataset.square;
+    console.log(num);
+    //console.log(state.board[num])
+    //console.log(state.board);
+
     if(state.board[event.target.dataset.square] == ''){
 
       state.board[event.target.dataset.square] = state.turn;
-  
+      
       event.target.innerText = state.turn;
       setState({
         turn: state.turn == 'X' ? 'O' : 'X',
         board: state.board,
+        totalMoves: state.totalMoves++,
       })
       
     }
+    
+    let result = checkWinner();
 
-    console.log(state.board);
+    if(result == 'X'){
+      setState({
+        gameEnded: true,
+        winner: 'X',
+      })
+    } else {
+      setState({
+        gameEnded: true,
+        winner: 'O',
+      })
+    }
+
   };
+
+  const checkWinner = () => {
+    let lines = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+
+    for(let i = 0; i < lines.length; i++){
+      if(state.board[lines[i][0]] == state.board[lines[i][1]] && state.board[lines[i][1]] == state.board[lines[i][2]]) {
+        return state.board[lines[i][0]];
+      }
+    }
+  }
 
   return (
     <div className="flex w-72 flex-wrap mt-6 mb-6 cursor-pointer" onClick={(e) => clicked(e)}>
